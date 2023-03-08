a218595570caece45252c1695214a27cd17c4c84
Selvi <reachyourgoal2006@gmail.com>
2023-03-08 20:09:39 +0530

    bug fix for path

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const path = require('path')
 
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(__dirname,'..','views','add-product.html'));
      
   });
   
